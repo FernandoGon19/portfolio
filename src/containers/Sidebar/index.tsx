42e7bfc5560ef5b6b86af678fa15cd325b54fa32
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2023-12-31 23:31:09 -0300

    finalizando projeto

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,14 +3,18 @@ import Avatar from "../../components/Avatar"
 import Paragrafo from "../../components/Paragrafo"
 import { Descricao, BotaoTema,  SidebarContainer } from './styles'
 
-const Sidebar = () => (
+type Props = {
+    trocaTema: () => void;
+}
+
+const Sidebar = (props: Props) => (
     <aside>
         <SidebarContainer>
             <Avatar></Avatar>
             <Titulo fontSize={20}>Fernando</Titulo>
             <Paragrafo tipo="secundario" fontSize={16}>FernandoGon19</Paragrafo>
             <Descricao tipo="principal" fontSize={12}>Engenheiro Front-End</Descricao>
-            <BotaoTema>Trocar tema</BotaoTema>
+            <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
         </SidebarContainer>
         
     </aside>
